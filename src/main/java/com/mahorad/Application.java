4d2dd71e54821a43abd5165240f22fedb43077b3
Mahan Rad <mahorad@yahoo.com>
2017-10-13 07:24:24 +0200

    add static contents when service starts up

--- src/main/java/com/mahorad/Application.java
+++ src/main/java/com/mahorad/Application.java
@@ -1,7 +1,13 @@
 package com.mahorad;
 
+import com.mahorad.model.IVeggieRepository;
+import com.mahorad.model.Veggie;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +16,14 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public CommandLineRunner setup(IVeggieRepository repository) {
+        return (args) -> {
+            repository.save(new Veggie("1", "Lettuce", new BigDecimal("7.80")));
+            repository.save(new Veggie("2", "Cabbage", new BigDecimal("5.99")));
+            repository.save(new Veggie("3", "Celery", new BigDecimal("9.92")));
+            repository.save(new Veggie("4", "Tomato", new BigDecimal("11.20")));
+            repository.save(new Veggie("5", "Broccoli", new BigDecimal("4.99")));
+        };
+    }
 }
